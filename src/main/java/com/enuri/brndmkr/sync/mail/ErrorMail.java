f92c44f3311dd9303e2581124e77f08c3261a984
AnJaeKoo <AnJaeKoo@AnJaeKoo>
2022-01-24 15:38:32 +0900

    .

--- src/main/java/com/enuri/brndmkr/sync/mail/ErrorMail.java
+++ src/main/java/com/enuri/brndmkr/sync/mail/ErrorMail.java
@@ -10,16 +10,16 @@ public class ErrorMail {
 	 * Exception 발생 시 관리자에게 Mail 보냄.
 	 ***************************************************************/
 	public ErrorMail(Exception e) {
-		String receiverMailAddr = "ajk777@enuri.com";				//받는 사람 메일주소
-		String receiverMailNm = "안재구";								//받는 사람 이름
+		String receiverMailAddr = "";				//받는 사람 메일주소
+		String receiverMailNm = "개발자";								//받는 사람 이름
 		String mailTitle = "[그룹매칭_브랜드싱크] " + e.getMessage();		//타이틀
 		String mailContents = "";									//내용
 
 		mailContents = mailContents + "re : <br/> " + e.toString();
 		mailContents = mailContents + "<br/><br/> re.getLocalizedMessage() : <br/> " + e.getLocalizedMessage();
 
-		String sendMailAddr = "ajk777@enuri.com";			//보내는 사람 메일주소
-		String sendMailNm = "안재구";							//보내는 사람 이름
+		String sendMailAddr = "";			//보내는 사람 메일주소
+		String sendMailNm = "개발자";							//보내는 사람 이름
 
 		SendMail_Data sendMailData = new SendMail_Data();
 		SendMail sendMail = new SendMail();
--- src/main/java/com/enuri/brndmkr/sync/mail/SuccessMail.java
+++ src/main/java/com/enuri/brndmkr/sync/mail/SuccessMail.java
@@ -13,15 +13,15 @@ public class SuccessMail {
 	 * 성공 메일
 	 ***************************************************************/
 	public void sendMail(String msg) {
-		String receiverMailAddr = "ajk777@enuri.com";					//받는 사람 메일주소
-		String receiverMailNm = "안재구";										//받는 사람 이름
+		String receiverMailAddr = "";					//받는 사람 메일주소
+		String receiverMailNm = "개발자";										//받는 사람 이름
 		String mailTitle = "[그룹매칭_브랜드/제조사 싱크] SUCCESS";	//타이틀
 		String mailContents = "";											//내용
 
 		mailContents = msg;
 
-		String sendMailAddr = "ajk777@enuri.com";						//보내는 사람 메일주소
-		String sendMailNm = "안재구";											//보내는 사람 이름
+		String sendMailAddr = "";						//보내는 사람 메일주소
+		String sendMailNm = "개발자";											//보내는 사람 이름
 
 		SendMail_Data sendMailData = new SendMail_Data();
 		SendMail sendMail = new SendMail();
@@ -37,13 +37,13 @@ public class SuccessMail {
 			sendMail.setHtmlMailSendProc(sendMailData);
 
 			//참고 두번째 수신인 강제 적용 : 많이 안 넣게 설계함
-			sendMailData.setReceiverMailAddr("jisunny1215@enuri.com");
-			sendMailData.setReceiverMailNm("이지선");
+			sendMailData.setReceiverMailAddr("");
+			sendMailData.setReceiverMailNm("담당자1");
 			sendMail.setHtmlMailSendProc(sendMailData);
 
 			//참고 세번째 수신인 강제 적용 : 많이 안 넣게 설계함
-			sendMailData.setReceiverMailAddr("ksa1132@enuri.com");
-			sendMailData.setReceiverMailNm("공승안");
+			sendMailData.setReceiverMailAddr("");
+			sendMailData.setReceiverMailNm("담당자2");
 			sendMail.setHtmlMailSendProc(sendMailData);
 
 		} catch (Exception e) {
